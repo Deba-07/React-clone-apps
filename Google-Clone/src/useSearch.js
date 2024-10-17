fa93d9d5fbc42feb020c2a8364ec8274d3f0fcd8
Deba-07 <dynamicdeba07@gmail.com>
2024-10-17 17:46:31 +0530

    updated keys.js

--- Google-Clone/src/useSearch.js
+++ Google-Clone/src/useSearch.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import API_KEY from "./keys";
-const CONTEXT_KEY = ""
+const CONTEXT_KEY = "b0c680006862243d3"
 
 const useGoogleSearch = (term) => {
     const [data, setData] = useState(null)
